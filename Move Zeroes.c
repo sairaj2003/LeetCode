6e906774406298f8a3de75987b05cdb05936c6d1
Sairaj Shirole <121667877+sairaj2003@users.noreply.github.com>
2023-08-16 23:26:59 +0530

    Create Move Zeroes.c

---
+++ Move Zeroes.c
@@ -0,0 +1,32 @@
+/*
+Given an integer array nums, move all 0's to the end of it while maintaining the relative order of the non-zero elements.
+
+Note that you must do this in-place without making a copy of the array.
+
+Example 1:
+
+Input: nums = [0,1,0,3,12]
+Output: [1,3,12,0,0]
+Example 2:
+
+Input: nums = [0]
+Output: [0]
+*/
+
+void moveZeroes(int* nums, int numsSize) {
+    int nonzero=0;
+    for(int i=0;i<numsSize;i++)
+    {
+        if(nums[i] != 0)
+        {
+            nums[nonzero]=nums[i]; 
+            nonzero++;
+        }
+    }
+
+    for(int i=nonzero;i<numsSize;i++)
+    {
+        nums[i]=0;
+        nonzero++;
+    }
+}
