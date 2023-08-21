428b4c10b1c3da94c2aa827342bff72f59fb2d23
Sairaj Shirole <121667877+sairaj2003@users.noreply.github.com>
2023-08-21 18:21:42 +0530

    Create Search Insert Position.c

---
+++ Search Insert Position.c
@@ -0,0 +1,47 @@
+/*
+Search Insert Position
+
+Given a sorted array of distinct integers and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
+
+You must write an algorithm with O(log n) runtime complexity.
+
+ 
+
+Example 1:
+
+Input: nums = [1,3,5,6], target = 5
+Output: 2
+Example 2:
+
+Input: nums = [1,3,5,6], target = 2
+Output: 1
+Example 3:
+
+Input: nums = [1,3,5,6], target = 7
+Output: 4
+*/
+
+int searchInsert(int* nums, int numsSize, int target){
+
+    int left=0;
+    int right=numsSize-1;
+
+    while(left <= right)
+    {
+        int mid= (left+right) / 2;
+
+        if(target == nums[mid])
+        {
+            return mid;
+        }
+        else if(target > nums[mid])
+        {
+            left=mid+1;
+        }
+        else if(target < nums[mid])
+        {
+            right=mid-1;
+        }
+    }
+    return left;
+}
